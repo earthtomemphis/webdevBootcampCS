fcfa00b62ce5932f6ec3af95bf91881c6bb4671a
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-04-12 16:14:11 -0600

    score keeper refactored

--- 09_score_keeper/app.js
+++ 09_score_keeper/app.js
@@ -1,42 +1,61 @@
-const p1Btn = document.querySelector('#p1Btn');
-const p2Btn = document.querySelector('#p2Btn');
-const resetBtn = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const winningScoreSelect = document.querySelector('#playto');
+const p1 = {
+	score: 0,
+	button: document.querySelector('#p1Btn'),
+	display: document.querySelector('#p1Display'),
+};
+const p2 = {
+	score: 0,
+	button: document.querySelector('#p2Btn'),
+	display: document.querySelector('#p2Display'),
+};
 
-let p1Score = 0;
-let p2Score = 0;
+const resetBtn = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 let winningScore = 3;
 let isGameOver = false;
 
-p1Btn.addEventListener('click', function () {
+function updateScores(player, opponent) {
 	if (!isGameOver) {
-		p1Score += 1;
-		if (p1Score === winningScore) {
+		player.score += 1;
+		if (player.score === winningScore) {
 			isGameOver = true;
-			p1Display.classList.add('has-text-success');
-			p2Display.classList.add('has-text-danger');
-			p1Btn.disabled = true;
-			p2Btn.disabled = true;
+			player.display.classList.add('has-text-success');
+			opponent.display.classList.add('has-text-danger');
+			player.button.disabled = true;
+			opponent.button.disabled = true;
 		}
-		p1Display.textContent = p1Score;
+		player.display.textContent = player.score;
 	}
-});
-p2Btn.addEventListener('click', function () {
-	if (!isGameOver) {
-		p2Score += 1;
-		if (p2Score === winningScore) {
-			isGameOver = true;
-			p2Display.classList.add('has-text-success');
-			p1Display.classList.add('has-text-danger');
-			p1Btn.disabled = true;
-			p2Btn.disabled = true;
-		}
-		p2Display.textContent = p2Score;
-	}
-});
+}
 
+p1.button.addEventListener('click', function () {
+	updateScores(p1, p2);
+});
+p2.button.addEventListener('click', function () {
+	updateScores(p2, p1);
+});
+// if (!isGameOver) {
+// 	p1Score += 1;
+// 	if (p1Score === winningScore) {
+// 		isGameOver = true;
+// 		p1Display.classList.add('has-text-success');
+// 		p2Display.classList.add('has-text-danger');
+// 		p1Btn.disabled = true;
+// 		p2Btn.disabled = true;
+// 	}
+// 	p1Display.textContent = p1Score;
+// }
+// if (!isGameOver) {
+// 	p2Score += 1;
+// 	if (p2Score === winningScore) {
+// 		isGameOver = true;
+// 		p2Display.classList.add('has-text-success');
+// 		p1Display.classList.add('has-text-danger');
+// 		p1Btn.disabled = true;
+// 		p2Btn.disabled = true;
+// 	}
+// 	p2Display.textContent = p2Score;
+// }
 winningScoreSelect.addEventListener('change', function () {
 	winningScore = parseInt(this.value);
 	reset();
@@ -48,12 +67,32 @@ resetBtn.addEventListener('click', reset);
 
 function reset() {
 	isGameOver = false;
-	p1Score = 0;
-	p2Score = 0;
-	p1Display.textContent = 0;
-	p2Display.textContent = 0;
-	p1Display.classList.remove('has-text-success', 'has-text-danger');
-	p2Display.classList.remove('has-text-success', 'has-text-danger');
-	p1Btn.disabled = false;
-	p2Btn.disabled = false;
+	for (let p of [p1, p2]) {
+		p.score = 0;
+		p.display.textContent = 0;
+		p.display.classList.remove('has-text-success', 'has-text-danger');
+		p.button.disabled = false;
+	}
 }
+// function reset() {
+// 	isGameOver = false;
+// 	p1.score = 0;
+// 	p2.score = 0;
+// 	p1.display.textContent = 0;
+// 	p2.display.textContent = 0;
+// 	p1.display.classList.remove('has-text-success', 'has-text-danger');
+// 	p2.display.classList.remove('has-text-success', 'has-text-danger');
+// 	p1.button.disabled = false;
+// 	p2.button.disabled = false;
+// }
+// function reset() {
+// 	isGameOver = false;
+// 	p1Score = 0;
+// 	p2Score = 0;
+// 	p1Display.textContent = 0;
+// 	p2Display.textContent = 0;
+// 	p1Display.classList.remove('has-text-success', 'has-text-danger');
+// 	p2Display.classList.remove('has-text-success', 'has-text-danger');
+// 	p1Btn.disabled = false;
+// 	p2Btn.disabled = false;
+// }
