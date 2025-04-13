dac500d964067782b8ce461221c333aec19d11eb
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-04-12 18:45:05 -0600

    single treaded

--- 10_async_js/01_callstack/app.js
+++ 10_async_js/01_callstack/app.js
@@ -7,3 +7,9 @@ const isRightTriangle = (a, b, c) => {
 };
 
 isRightTriangle(3, 4, 5); // returns true
+
+console.log('Sending request to server!');
+setTimeout(() => {
+	console.log('Here is your data from the server');
+}, 3000);
+console.log(' I AM AT THE END OF THE FILE');
