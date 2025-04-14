10378d20c5cde744ea7678b25cf81cedb0a86d2f
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-04-13 19:02:34 -0600

    fake request promise

--- 10_async_js/03_PromisesIntro/app.js
+++ 10_async_js/03_PromisesIntro/app.js
@@ -33,33 +33,113 @@ const fakeRequestPromise = (url) => {
 // 		console.log('ERROR!!!', err);
 // 	}
 // );
-fakeRequestCallback(
-	'books.com/page1',
-	function (response) {
-		console.log('IT WORKED!!!');
-		console.log(response);
-		fakeRequestCallback(
-			'books.com/page2',
-			function (response) {
-				console.log('IT WORKED AGAIN!!!');
-				console.log(response);
-				fakeRequestCallback(
-					'books.com/page3',
-					function (response) {
-						console.log('IT WORKED AGAIN (3rd req)!!!');
-						console.log(response);
-					},
-					function (err) {
-						console.log('ERROR (3rd req)!!!', err);
-					}
-				);
-			},
-			function (err) {
-				console.log('ERROR (2nd req)!!!', err);
-			}
-		);
-	},
-	function (err) {
-		console.log('ERROR!!!', err);
-	}
-);
+// fakeRequestCallback(
+// 	'books.com/page1',
+// 	function (response) {
+// 		console.log('IT WORKED!!!');
+// 		console.log(response);
+// 		fakeRequestCallback(
+// 			'books.com/page2',
+// 			function (response) {
+// 				console.log('IT WORKED AGAIN!!!');
+// 				console.log(response);
+// 				fakeRequestCallback(
+// 					'books.com/page3',
+// 					function (response) {
+// 						console.log('IT WORKED AGAIN (3rd req)!!!');
+// 						console.log(response);
+// 					},
+// 					function (err) {
+// 						console.log('ERROR (3rd req)!!!', err);
+// 					}
+// 				);
+// 			},
+// 			function (err) {
+// 				console.log('ERROR (2nd req)!!!', err);
+// 			}
+// 		);
+// 	},
+// 	function (err) {
+// 		console.log('ERROR!!!', err);
+// 	}
+// );
+
+// const request = fakeRequestPromise('yelp.com/api/coffee');
+// request
+// 	.then(() => {
+// 		console.log('PROMISE RESOLVED!');
+// 		console.log('IT WORKED!!!!!!');
+// 	})
+// 	.catch(() => {
+// 		console.log('PROMISE REJECTED!');
+// 		console.log('OH NO, ERROR!!!');
+// 	});
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+// 	.then(() => {
+// 		console.log('PROMISE RESOLVED! (page1)');
+// 		console.log('IT WORKED!!!!!!');
+// 		fakeRequestPromise('yelp.com/api/coffee/page2')
+// 			.then(() => {
+// 				console.log('PROMISE RESOLVED! (page2)');
+// 				console.log('IT WORKED!!!!!!');
+// 				fakeRequestPromise('yelp.com/api/coffee/page3')
+// 					.then(() => {
+// 						console.log('PROMISE RESOLVED! (page3)');
+// 						console.log('IT WORKED!!!!!!');
+// 					})
+// 					.catch(() => {
+// 						console.log('PROMISE REJECTED! (page3)');
+// 						console.log('OH NO, ERROR!!!');
+// 					});
+// 			})
+// 			.catch(() => {
+// 				console.log('PROMISE REJECTED! (page2)');
+// 				console.log('OH NO, ERROR!!!');
+// 			});
+// 	})
+// 	.catch(() => {
+// 		console.log('PROMISE REJECTED! (page1)');
+// 		console.log('OH NO, ERROR!!!');
+// 	});
+
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+// 	.then(() => {
+// 		console.log('PROMISE RESOLVED! (page1)');
+// 		console.log('IT WORKED!!!!!!');
+// 		return fakeRequestPromise('yelp.com/api/coffee/page2');
+// 	})
+// 	.then(() => {
+// 		console.log('PROMISE RESOLVED! (page2)');
+// 		console.log('IT WORKED!!!!!!');
+// 		return fakeRequestPromise('yelp.com/api/coffee/page3');
+// 	})
+// 	.then(() => {
+// 		console.log('PROMISE RESOLVED! (page3)');
+// 		console.log('IT WORKED!!!!!!');
+// 	})
+// 	.catch(() => {
+// 		console.log('OH NO, A REQUEST FAILED!!!');
+// 	});
+
+fakeRequestPromise('yelp.com/api/coffee/page1')
+	.then((data) => {
+		console.log('PROMISE RESOLVED! (page1)');
+		console.log('IT WORKED!!!!!!');
+		console.log(data);
+		return fakeRequestPromise('yelp.com/api/coffee/page2');
+	})
+	.then((data) => {
+		console.log('PROMISE RESOLVED! (page2)');
+		console.log('IT WORKED!!!!!!');
+		console.log(data);
+		return fakeRequestPromise('yelp.com/api/coffee/page3');
+	})
+	.then((data) => {
+		console.log('PROMISE RESOLVED! (page3)');
+		console.log('IT WORKED!!!!!!');
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log('OH NO, A REQUEST FAILED!!!');
+		console.log(err);
+	});
