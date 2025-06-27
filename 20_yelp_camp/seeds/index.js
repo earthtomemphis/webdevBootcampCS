038a1a1f6d37d568e47f5fe1d2b829483468f4b1
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-06-27 14:02:10 +0700

    yelp camp styles

--- 20_yelp_camp/seeds/index.js
+++ 20_yelp_camp/seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const rand1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: `https:picsum.photos/400?random=${Math.random()}`,
+			description:
+				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur soluta dolor voluptatum necessitatibus nobis hic vero excepturi cumque, facere expedita, commodi maxime amet dolorem ex eius quo illum porro! Voluptates.',
+			price,
 		});
 		await camp.save();
 	}
